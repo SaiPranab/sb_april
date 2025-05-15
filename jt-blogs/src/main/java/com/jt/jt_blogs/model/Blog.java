acfa71f851b91f44ed3a143bd6b2ba4f511c1906
SaiPranab <saipranabpatra1234@gmail.com>
2025-05-15 20:55:49 +0530

    Spring Data JPA dependency,  methods and usage

--- jt-blogs/src/main/java/com/jt/jt_blogs/model/Blog.java
+++ jt-blogs/src/main/java/com/jt/jt_blogs/model/Blog.java
@@ -1,5 +1,9 @@
 package com.jt.jt_blogs.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -9,7 +13,10 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
 public class Blog {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String heading;
     private String description;
--- jt-blogs/src/main/java/com/jt/jt_blogs/service/BlogService.java
+++ jt-blogs/src/main/java/com/jt/jt_blogs/service/BlogService.java
@@ -1,6 +1,7 @@
 package com.jt.jt_blogs.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
@@ -8,38 +9,30 @@ import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Service;
 
 import com.jt.jt_blogs.model.Blog;
+import com.jt.jt_blogs.repository.BlogRepository;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
 public class BlogService {
-        private static final String BLOG_TABLE = "blogs";
+        private static final String BLOG_TABLE = "blog";
         private final JdbcTemplate jdbcTemplate;
+        private final BlogRepository blogRepository;
 
         public List<Blog> getBlogs() {
-                var sql = "SELECT * FROM " + BLOG_TABLE;
-                return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(Blog.class));
+                return blogRepository.findAll(); // findAll() - extract all rows from the table
         }
 
         public void addBlog(Blog blog) {
-                var sql = "INSERT INTO " + BLOG_TABLE + " (heading, description) VALUES (?, ?)";
-                jdbcTemplate.update(sql, blog.getHeading(), blog.getDescription());
+                blogRepository.save(blog); // save - insert row into table
         }
 
         public Blog getBlogById(int id) {
-                var sql = "SELECT * FROM " + BLOG_TABLE + " WHERE id = " + id;
+                return blogRepository.findById(id)
+                                .orElseThrow(() -> new RuntimeException("Blog not found with id " + id));
 
-                RowMapper<Blog> rowMapper = (resultSet, rowNum) -> {
-                        return new Blog(
-                                        resultSet.getInt("id"),
-                                        resultSet.getString(2),
-                                        resultSet.getString("description"));
-                };
-                Blog blog = jdbcTemplate.queryForObject(sql, rowMapper);
-                return blog;
-
-                // jdbcTemplate.queryForObject(sql, new BeanPropertyRowMapper<>(Blog.class));
+                // findById() -> it extract a row by matching the primary key
         }
 
         public void delete(int id) {
